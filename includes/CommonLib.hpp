6f2486483e88c4c295eca3e9a71223ba38b18301
dieriba <81977367+dieriba@users.noreply.github.com>
2023-04-12 11:56:39 +0200

    Update CommonLib.hpp remove bitswap

--- includes/CommonLib.hpp
+++ includes/CommonLib.hpp
@@ -2,8 +2,7 @@
 #define __HEADER__HPP_
 #include <string>
 #include <iostream>
-#define bitset(byte,nbit)   ((byte) |=  (1<<(nbit)))
-#define bitclear(byte,nbit) ((byte) &= ~(1<<(nbit)))
-#define bitflip(byte,nbit)  ((byte) ^=  (1<<(nbit)))
-#define bitcheck(byte,nbit) ((byte) &   (1<<(nbit)))
+#define bitset(byte,nbit)   (byte |= (1 << nbit))
+#define bitclear(byte,nbit) (byte &= ~(1 << nbit))
+#define bitcheck(byte,nbit) (byte & (1 << nbit))
 #endif
