ad1e333ad2fbad17aa6d8baf3d0840f0b60b83bb
Dieriba TOURE <dtoure@e2r5p21.clusters.42paris.fr>
2023-04-14 20:05:47 +0200

    new update

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -3,6 +3,9 @@
 #include "CommonLib.hpp"
 #include "BitsManipulation.hpp"
 #include "TcpServer.hpp"
+
+typedef std::vector<std::string>::iterator it_vec;
+
 class Location;
 class Server: public BitsManipulation, public TcpServer
 {
@@ -13,6 +16,7 @@ class Server: public BitsManipulation, public TcpServer
         ~Server();
 
         /*GETTERS*/
+        const it_vec& getCgiPath(const std::string& key);
         const unsigned int& getPort(void) const;
         std::vector<Location> getLocations(void) const;
         std::vector<std::string> getServerNames(void) const;
@@ -21,10 +25,12 @@ class Server: public BitsManipulation, public TcpServer
         void    setPort(const unsigned int& port);
         void    pushNewServerName(const std::string& server_name);
         void    pushNewLocation(const Location& server_name);
+        void    pushNewCGI(const std::string& value);
 
     private:
         unsigned int    _port;
         std::vector<std::string> _server_names;
+        std::vector<std::string> _cgi;
         std::vector<Location> _locations;
 };
 #endif
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -25,12 +25,23 @@ Server::~Server(){};
 /*----------------------------------------CONSTRUCTOR/DESTRUCTOR----------------------------------------*/
 
 /*----------------------------------------GETTER----------------------------------------*/
+const it_vec& Server::getCgiPath(const std::string& key)
+{
+    for (it_vec i = _cgi.begin(); i != _cgi.end(); i++)
+    {
+        if (*i == key)
+            return (i);
+    }
+
+    return _cgi.end();
+};
 const unsigned int& Server::getPort(void) const {return _port;};
 std::vector<Location> Server::getLocations(void) const {return _locations;};
 std::vector<std::string> Server::getServerNames(void) const {return _server_names;};
 /*----------------------------------------GETTER----------------------------------------*/
 
 /*----------------------------------------SETTER----------------------------------------*/
+
 void    Server::setPort(const unsigned int& port)
 {
     _port = port;
@@ -45,6 +56,11 @@ void    Server::pushNewLocation(const Location& location)
 {
     _locations.push_back(location);
 };
+
+void    Server::pushNewCGI(const std::string& value)
+{
+    _cgi.push_back(value);
+}
 
 /*----------------------------------------SETTER----------------------------------------*/
 
