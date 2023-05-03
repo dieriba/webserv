95f21c973c791d826d8d1d07c771f4fbc6a2f818
dieriba <dieriba.pro@gmail.com>
2023-05-03 18:04:23 +0200

    test

--- includes/method/Get.hpp
+++ includes/method/Get.hpp
@@ -15,6 +15,9 @@ class Get: public Method
         virtual ~Get();
 
         void sendResponse(const IO&, const HttpRequest&);
+    private:
+        int handleFileRessource(const IO& event, const HttpRequest& req, std::string& ressource);
+        int handleDirectoryRessource(const IO& event, DIR *directory);
 };
 
 
--- srcs/method/Get.cpp
+++ srcs/method/Get.cpp
@@ -21,17 +21,51 @@ Get::~Get(){};
 /*----------------------------------------SETTER----------------------------------------*/
 /*----------------------------------------SETTER----------------------------------------*/
 
+
 /*----------------------------------------MEMBER FUNCTION----------------------------------------*/
+int Get::handleFileRessource(const IO& event, const HttpRequest& req, std::string& ressource)
+{
+    int status = event.getErrStatus();
+    std::ifstream file;
+    std::ostringstream ss;
+    
+    if (status == 0)
+    {
+        file.open(ressource.c_str(), std::ios::binary);
+
+        status = !file ? FORBIDEN : OK;
+
+        ressource.clear();
+    }
+
+    ss << status;
+    std::string code(ss.str());
+    ressource = req.getHeaders().find(VERSION) -> second + " "\
+                + code + " " + TcpServer::getHttpResponse(status) -> second + CRLF;
+    if ((event.getErrStatus()) || (event.getErrStatus() == 0 && !file)) return 1;
+     
+    return 0;
+}
+
+int Get::handleDirectoryRessource(const IO& event, DIR *directory)
+{
+    (void)event;
+    (void)directory;
+    return 0;
+}
+
 void Get::sendResponse(const IO& event, const HttpRequest& req)
 {
     TcpServer& instance = *(event.getServer() -> getInstance());
     std::string ressource(instance.getRootDir() + req.getHeaders().find(PATH) -> second);
     DIR *directory = opendir(ressource.c_str());
-    std::cout << ressource << std::endl;
-    if (directory == NULL && errno == ENOENT)
-        std::cout << "The Given path: " << ressource << " ain't a directory" << std::endl;
+    if (directory == NULL && (errno == ENOENT || errno == ENOTDIR))
+    {
+        handleFileRessource(event, req, ressource);
+    }
     else
-        std::cout << "Directory at: " << ressource << " openened" << std::endl;
+        handleDirectoryRessource(event, directory);
+    std::cout << ressource;
     exit(1);
 }
 /*----------------------------------------MEMBER FUNCTION----------------------------------------*/
